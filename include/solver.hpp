c26b257f9f020262982f752debbdb45eff36651b
Jakub Prokopiuk <kubapro84@wp.pl>
2025-04-01 23:20:06 +0200

    changed ifndef to pragma once

--- include/solver.hpp
+++ include/solver.hpp
@@ -1,5 +1,4 @@
-#ifndef SOLVER_HPP
-#define SOLVER_HPP
+#pragma once
 
 #include "board.hpp"
 #include <queue>
@@ -15,5 +14,3 @@ private:
     Board initialBoard;
     std::string serialize(const Board& board) const;
 };
-
-#endif
